955af8a8083637aebe8ab5c818cdf5cd30ad1ad1
ajwad_imran <ajwadimran7@gmail.com>
2019-01-30 20:20:15 +0500

    Project setup + Assign a destination to agents from a pool of points.

---
+++ Assets/Scripts/AI/RoamingAI.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class RoamingAI : MonoBehaviour {
+
+    NavMeshAgent _agent;
+	GameObject _destination;
+    bool _hasDestination = false;
+
+	void Start () {
+		_agent = GetComponent<NavMeshAgent> ();
+        _destination = NPCManager.Instance.getDestination(this);
+        moveToDestination();
+	}
+
+    void Update()
+    {
+        if(_hasDestination) {
+            if(Vector3.Distance(transform.position, _destination.transform.position) <= 1f) {
+                reachedDestination();
+            }
+        }
+    }
+	
+    /// <summary>
+    /// Allows the NPC Manager to send this agent a destination if it was not previously available.
+    /// </summary>
+	public void SetPatrolPoint (GameObject destination) {
+        _destination = destination;
+        moveToDestination();
+	}
+
+    /// <summary>
+    /// Set the nav mesh destionation.
+    /// </summary>
+    void moveToDestination () {
+        if(_destination) {
+            _hasDestination = true;
+            _agent.SetDestination(_destination.transform.position);
+        }
+    }
+
+    /// <summary>
+    /// Post destination arrival operation.
+    /// </summary>
+    void reachedDestination () {
+        _hasDestination = false;
+        NPCManager.Instance.returnDestination(_destination);
+        _destination = NPCManager.Instance.getDestination(this);
+        moveToDestination();
+    }
+}
---
+++ Assets/Scripts/Managers/NPCManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCManager : MonoBehaviour {
+
+    public static NPCManager Instance;
+    public List<GameObject> patrolPoints = new List<GameObject> ();
+    List<RoamingAI> _agentsQueue = new List<RoamingAI> ();
+
+    void Awake () {
+        Instance = this;
+    }
+
+    /// <summary>
+    /// Sends a patrol point to agent. If all patrol points are taken then it will add the agent to the queue
+    /// and wait for a patrol point to be free.
+    /// </summary>
+    public GameObject getDestination (RoamingAI agent) {
+        if(patrolPoints.Count > 0) {
+            GameObject tempDestination = patrolPoints[0];
+            patrolPoints.RemoveAt(0);
+            return tempDestination;
+        } else {
+            _agentsQueue.Add(agent);
+            return null;
+        }
+    }
+
+    /// <summary>
+    /// Return the patrol point back to the list after agent has reached it.
+    /// </summary>
+    public void returnDestination (GameObject destination) {
+        if(_agentsQueue.Count > 0) {
+            _agentsQueue[0].SetPatrolPoint (destination);
+            _agentsQueue.RemoveAt(0);
+        } else {
+            patrolPoints.Add(destination);
+        }
+    }
+}
